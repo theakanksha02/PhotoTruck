b71d6153adf9a5e1981bb8e3e9c23692e30ec867
theakanksha02 <theakankshaa@gmail.com>
2017-05-22 17:53:10 +0530

    first commit

--- app/src/main/java/com/bignerdranch/android/phototruck/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/phototruck/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.bignerdranch.android.phototruck;
 
 import android.app.WallpaperManager;
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager layoutManager;
 
     ArrayList<String> mCategoryList;
+    List<Integer> mCurrentWallpaperList;
 
 
 
@@ -45,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
         rvGrid = (RecyclerView) findViewById(R.id.rv_grid);
         rvCategory = (RecyclerView) findViewById(R.id.rv_category);
 
-
         mCategoryList = new ArrayList<String>();
         mCategoryList.add("Nature");
         mCategoryList.add("Abstract");
@@ -54,29 +55,9 @@ public class MainActivity extends AppCompatActivity {
         mCategoryList.add("Gadgets");
 
 
-        final List<Integer> wallList = new ArrayList<>();
-        wallList.add(R.drawable.wall1);
-        wallList.add(R.drawable.wall2);
-        wallList.add(R.drawable.wall3);
-        wallList.add(R.drawable.wall4);
-        wallList.add(R.drawable.wall7);
-        wallList.add(R.drawable.wall8);
-        wallList.add(R.drawable.wall9);
-        wallList.add(R.drawable.wall10);
-        wallList.add(R.drawable.wall11);
-        wallList.add(R.drawable.wall12);
-        wallList.add(R.drawable.wall13);
-        wallList.add(R.drawable.wall15);
-        wallList.add(R.drawable.wall16);
-        wallList.add(R.drawable.wall17);
 
 
 
-        adapter = new WallpaperAdapter(wallList, this);
-        layoutManager = new GridLayoutManager(MainActivity.this, 2);
-        rvGrid.setLayoutManager(layoutManager);
-        rvGrid.setAdapter(adapter);
-        rvGrid.setHasFixedSize(true);
 
         // for category
         rvCategory.setAdapter(new MainAdapter(this, mCategoryList));
@@ -85,8 +66,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClicked(RecyclerView recyclerView, int position, View v) {
                 Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+
                 List<Integer> wallpaperList = getWallpaperList(mCategoryList.get(position));
-                // TODO: 5/21/2017 set this wallpaperList to the rvGrid  
+                // here i have change
+
+
+                adapter = new WallpaperAdapter(mCurrentWallpaperList,MainActivity.this);
+                layoutManager = new GridLayoutManager(MainActivity.this, 2);
+                rvGrid.setLayoutManager(layoutManager);
+                rvGrid.setAdapter(adapter);
+                rvGrid.setHasFixedSize(true);
+
+
             }
         });
 
@@ -100,7 +91,9 @@ public class MainActivity extends AppCompatActivity {
                     public void onItemClicked(RecyclerView recyclerView, int position, View v) {
                         // here you handle what you want to do on the click of item.
                         // set the wallpaper here.
-                        int imageResource = wallList.get(position);
+
+                        // here you get the wallpaper user have clicked from mCurrentWallpaperList
+                        int imageResource = mCurrentWallpaperList.get(position);
                         setThisAsWallpaper(imageResource);
 
                     }
@@ -136,20 +129,101 @@ public class MainActivity extends AppCompatActivity {
      * category selected.
      * */
     public List<Integer> getWallpaperList(String category){
-        if (category.equalsIgnoreCase("spiritual")){
-            // TODO: 5/21/2017 make a list of wallpaper based on category and return 
+        if (category.equalsIgnoreCase("nature")){
+
             final List<Integer> wallList = new ArrayList<>();
             wallList.add(R.drawable.wall1);
             wallList.add(R.drawable.wall2);
             wallList.add(R.drawable.wall3);
             wallList.add(R.drawable.wall4);
+            wallList.add(R.drawable.wall5);
+            wallList.add(R.drawable.wall6);
             wallList.add(R.drawable.wall7);
+            wallList.add(R.drawable.wall8);
+            wallList.add(R.drawable.wall9);
+            wallList.add(R.drawable.wall10);
+
             return  wallList;
-        } else if(category.equalsIgnoreCase("nature")){
-            return null;
+        } else if(category.equalsIgnoreCase("Abstract")){
+
+            final List<Integer> wallList = new ArrayList<>();
+            wallList.add(R.drawable.wall11);
+            wallList.add(R.drawable.wall12);
+            wallList.add(R.drawable.wall13);
+            wallList.add(R.drawable.wall14);
+            wallList.add(R.drawable.wall15);
+            wallList.add(R.drawable.wall16);
+            wallList.add(R.drawable.wall17);
+            wallList.add(R.drawable.wall18);
+            wallList.add(R.drawable.wall19);
+            wallList.add(R.drawable.wall20);
+
+
+            return wallList;
         }
 
-        return null;
+        else if(category.equalsIgnoreCase("Spiritual")){
+
+            final List<Integer> wallList = new ArrayList<>();
+            wallList.add(R.drawable.wall21);
+            wallList.add(R.drawable.wall22);
+            wallList.add(R.drawable.wall23);
+            wallList.add(R.drawable.wall24);
+            wallList.add(R.drawable.wall25);
+            wallList.add(R.drawable.wall26);
+            wallList.add(R.drawable.wall27);
+            wallList.add(R.drawable.wall28);
+            wallList.add(R.drawable.wall29);
+            wallList.add(R.drawable.wall30);
+
+
+            return wallList;
+        }
+
+
+        else if(category.equalsIgnoreCase("Cartoons")){
+
+            final List<Integer> wallList = new ArrayList<>();
+            wallList.add(R.drawable.wall31);
+            wallList.add(R.drawable.wall32);
+            wallList.add(R.drawable.wall33);
+            wallList.add(R.drawable.wall34);
+            wallList.add(R.drawable.wall35);
+            wallList.add(R.drawable.wall36);
+            wallList.add(R.drawable.wall37);
+            wallList.add(R.drawable.wall38);
+            wallList.add(R.drawable.wall39);
+            wallList.add(R.drawable.wall40);
+            wallList.add(R.drawable.wall41);
+            wallList.add(R.drawable.wall42);
+
+
+            return wallList;
+        }
+
+
+        else if(category.equalsIgnoreCase("Gadgets")){
+
+            final List<Integer> wallList = new ArrayList<>();
+            wallList.add(R.drawable.wall43);
+            wallList.add(R.drawable.walll44);
+            wallList.add(R.drawable.wall45);
+            wallList.add(R.drawable.wall46);
+            wallList.add(R.drawable.wall47);
+            wallList.add(R.drawable.wall48);
+            wallList.add(R.drawable.wall49);
+            wallList.add(R.drawable.wall50);
+            wallList.add(R.drawable.wall51);
+            wallList.add(R.drawable.wall52);
+
+
+
+            return wallList;
+        }
+        else {
+
+            return null;
+        }
     }
 
 }
